da320a4fd07e16b108dc2bf6cba1da70b28f8848
Miguel (SACYL) <ariesmoo@gmail.com>
2017-01-18 15:05:23 +0100

    Retirada de Winston. Incorporacion DEBUG

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ const mongoose       = require('mongoose');
 const bodyParser     = require('body-parser'); // Para parsear peticiones HTTP
 const morgan         = require('morgan');
 const sriracha       = require('sriracha');
-const winston        = require('winston');
-const expressWinston = require('express-Winston');
+const debug          = require('debug')('pozi');
 const apiRouter      = require('./routes/apiRoutes');
 const generalRouter  = require('./routes/generalRoutes');
 const authUserRouter = require('./routes/authUserRoutes');
@@ -22,20 +21,7 @@ app.use('/admin', sriracha());
 
 // Si estamos en un entorno de desarrollo (no de produccion)
 if (!config.production){
-  app.use(expressWinston.logger({
-      transports: [
-        new winston.transports.Console({
-          json: true,
-          colorize: true
-        })
-      ],
-      meta: true, // optional: control whether you want to log the meta data about the request (default to true)
-      msg: 'HTTP {{req.method}} {{req.url}}', // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
-      expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
-      colorize: true, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
-      ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
-  }));
-
+  debug('aqui hay logueo hoyga !!');
   mongoose.set('debug', true);
 }
 
@@ -56,16 +42,6 @@ mongoose.connect(config.db, (err, res) => {
 app.use('/', generalRouter);
 app.use('/api', apiRouter);
 app.use('/user', authUserRouter);
-
-// Configruamos winston para logear los errores (despues de los routers claro)
-app.use(expressWinston.errorLogger({
-  transports: [
-    new winston.transports.Console({
-      json: true,
-      colorize: true
-    })
-  ]
-}));
 
 // Arrancamos la aplicacion en el puerto especificado
 app.listen(config.port, () => {
